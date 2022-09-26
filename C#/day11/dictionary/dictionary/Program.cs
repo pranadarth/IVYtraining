2255491464c58156b1d8c1c2022e46be62122ffc
pranadarth <103335118+pranadarth@users.noreply.github.com>
2022-09-27 00:29:54 +0530

    adding day 11 assignment

---
+++ C#/day11/dictionary/dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+class students
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, int> data = new Dictionary<int, int>();
+        Console.WriteLine("Enter the data:");
+        do
+        {
+            Console.Write("Student ID: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write("Total Mark: ");
+            int m = int.Parse(Console.ReadLine());
+            data.Add(i, m);
+            Console.WriteLine("Press 1 to add another data!");
+
+        }while(int.Parse(Console.ReadLine()) != 0);
+        Console.WriteLine("\nEntered data till now: ");
+        Console.WriteLine("IDs  Marks");
+        foreach (KeyValuePair<int,int> i in data)
+        Console.WriteLine((i.Key)+" " + (i.Value));
+        Console.WriteLine("\nThe Highest mark is "+ data.Values.Max());
+    }
+}
---
+++ C#/day11/ques3/ques3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class database
+{
+
+    public void genericmethod<T1, T2, T3>(T1 id, T2 name, T3 status)
+    {
+        Console.WriteLine(id + " , " + name + " , " + status);
+    }
+    static void Main(string[] args)
+    {
+        database p = new database();
+        Console.WriteLine("The List of Students ID, Name along with their status");
+        p.genericmethod<int, string, char>(1, "pranav", 'Y');
+        p.genericmethod<int, string, char>(2, "Lakshmi", 'N');
+        p.genericmethod<int, string, char>(3, "Priya", 'Y');
+        p.genericmethod<int, string, char>(4, "Guru", 'Y');
+        p.genericmethod<int, string, char>(5, "Madhan", 'N');
+
+    }
+}
