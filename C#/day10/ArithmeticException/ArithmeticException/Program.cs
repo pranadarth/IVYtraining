4c46ea05ea99dd04a8a2b0f07bd2d9913c223dfb
pranadarth <103335118+pranadarth@users.noreply.github.com>
2022-09-26 11:46:43 +0530

    adding assignments in new repo

---
+++ C#/day10/ArithmeticException/ArithmeticException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class expection
+{
+    public void calc(int n)
+    {
+        try
+        {
+            Console.WriteLine("Root of the given num:" + Math.Sqrt(n));
+            Console.WriteLine("Square of the given num:" + Math.Pow(n,2));
+            Console.WriteLine("Division of the given num from 5 :" );
+            for(int i = 5; i >= 0; i--)
+                Console.WriteLine("by {0} is {1}", i, n/i);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("The following error has been occured: " + e.Message);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        expection f = new expection();
+        f.calc(18);
+    }
+
+}
---
+++ C#/day10/University/University/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+class People
+{
+    protected int ID;
+    protected string bookname;
+
+}
+class student: People
+{
+    public void disp(int id,string bk,string s )
+    {
+        Console.WriteLine("\n\tStudents portal!!");
+        Console.WriteLine("You have taken a {0} book today. Your due is in 7 days.", bk);
+        Console.WriteLine("student with ID: {0} has been noted for attendance, You have 5 classes to attend", id);
+
+    }
+}
+class teacher: student
+{
+    public void disp( string bk, string t, int id)
+    {
+        Console.WriteLine("\n\tStaff portal!!");
+        Console.WriteLine("You have taken a {0} book today. Your due is in 30 days.", bk);
+        Console.WriteLine("Staff ID: {0} has been noted for attendance, You have 5 classes to take today", id);
+
+    }
+}
+class liberian : teacher
+{
+    public void disp( string s, int id, string bk)
+    {
+        Console.WriteLine("\n\tAdmin portal!!");
+        Console.WriteLine(" {0} book has been add in the library list!", bk);
+        Console.WriteLine(" ID: {0} has been noted for todays attendance", id);
+
+    }
+}
+class main
+{
+  
+    static void Main()
+    {
+        liberian l = new liberian();
+        Console.WriteLine("Welcome to library: ");
+        again:
+        Console.Write("Enter ID: ");
+        int ID = int.Parse(Console.ReadLine());
+
+        Console.Write("Book: ");
+        string book = Console.ReadLine();
+
+        Console.Write("Designation: ");
+        string s = Console.ReadLine();
+        switch (s)
+        {
+            case "student":
+                l.disp(ID, book, s);
+                break;
+            case "teacher":
+                l.disp(book, s, ID);
+                break;
+            case "liberian":
+                l.disp(s,ID, book);
+                break;
+            default:
+                Console.WriteLine("\nPlease enter your designation correctly!");
+                goto again;
+                break;
+;        }
+    }
+}
