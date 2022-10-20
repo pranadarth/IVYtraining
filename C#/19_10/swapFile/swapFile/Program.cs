990ff76a5fb4c32beeb4d8d28fdaea26641bf5b5
pranadarth <103335118+pranadarth@users.noreply.github.com>
2022-10-20 12:22:40 +0530

    adding Inbuilt Encrption method

--- C#/19_10/swapFile/swapFile/Program.cs
+++ C#/19_10/swapFile/swapFile/Program.cs
@@ -8,21 +8,22 @@
             Directory.CreateDirectory(path);
             FileStream fs = new FileStream(path + "\\file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             StreamWriter sw = new StreamWriter(fs);
-            sw.WriteLine("Hello Everyone,\n This is File1 contents \nWe will swap these contents to another File ");
+            sw.WriteLine("Hello Everyone,\n This is File1 contents \nWe will swap this File to another other directory ");
             sw.Close();
             fs.Close();
             path = "C:\\Users\\suresh.pranadarth\\folderSecond";
             Directory.CreateDirectory(path);
             fs = new FileStream(path + "\\file2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             sw = new StreamWriter(fs);
-            sw.WriteLine("Hello Everyone,\n This is File2 contents \nWe will swap these contents to another File ");
+            sw.WriteLine("Hello Everyone,\n This is File2 contents \nWe will swap this File to another directory ");
+            File.Encrypt("C:\\Users\\suresh.pranadarth\\folderFirst\\file1.txt");
             sw.Close();
             fs.Close();
             File.Move("C:\\Users\\suresh.pranadarth\\folderFirst\\file1.txt", "C:\\Users\\suresh.pranadarth\\folderSecond\\file1.txt");
-            File.Delete("C:\\Users\\suresh.pranadarth\\folderFirst\\file1.txt");
+           /* File.Delete("C:\\Users\\suresh.pranadarth\\folderFirst\\file1.txt");*/
             File.Move("C:\\Users\\suresh.pranadarth\\folderSecond\\file2.txt", "C:\\Users\\suresh.pranadarth\\folderFirst\\file2.txt");
-            File.Delete("C:\\Users\\suresh.pranadarth\\folderSecond\\file2.txt");
-            
+            /*File.Delete("C:\\Users\\suresh.pranadarth\\folderSecond\\file2.txt");*/
+
         }
     }
 }
