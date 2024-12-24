b22fa6212c2df3ebe28ad5678ec26c4aef193cbb
Pranadarth <pranadarth.suresh@idrive.com>
2024-12-24 18:22:05 +0530

    Updated game's logic

--- WPF/Game/Game/MainWindow.xaml.cs
+++ WPF/Game/Game/MainWindow.xaml.cs
@@ -1,104 +1,155 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Game
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private enum Direction { Left, Right, Down, Up, None };
-        private bool GameOver = false;
-        Direction currentDirection;
-        int start = 0;
-
-
+        private bool gameOver = false;
+        private bool canStop = false;
+        private Direction currentDirection = Direction.None;
+        private int speed = 1;
+        private Thread gameThread;
+        private Window currentWindow;
 
         public MainWindow()
         {
             InitializeComponent();
-            currentDirection= Direction.None;
-           
+            currentWindow = Application.Current.MainWindow;
+            StartGameThread();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if(Key.Right == e.Key) 
+            if (e.Key == Key.Right)
             {
-               currentDirection= Direction.Right;
+                currentDirection = Direction.Right;
             }
-            else if(Key.Left == e.Key)
+            else if (e.Key == Key.Left)
             {
-                currentDirection= Direction.Left;
+                currentDirection = Direction.Left;
             }
-            else if (Key.Down == e.Key)
+            else if (e.Key == Key.Down)
             {
                 currentDirection = Direction.Down;
             }
-            else if (Key.Up == e.Key)
+            else if (e.Key == Key.Up)
             {
                 currentDirection = Direction.Up;
             }
-            else if(Key.Escape == e.Key)
+            else if (e.Key == Key.Escape)
             {
-                GameOver = true;
+                gameOver = true;
+            }
+            else if (e.Key == Key.Space)
+            {
+                canStop = !canStop; 
+            }
+        }
+
+        private void StartGameThread()
+        {
+            gameThread = new Thread(GameLoop);
+            gameThread.IsBackground = true;
+            gameThread.Start();
+        }
+
+        private void GameLoop()
+        {
+            while (true)
+            {
+                if (gameOver)
+                {
+                    Dispatcher.Invoke(() => ResetGame());
+                }
+
+                if (!canStop)
+                {
+                    Dispatcher.Invoke(() =>  Movement());
+                }
+
+                Thread.Sleep(10); // Controls the update rate (20 FPS)
+            }
+        }
+
+        private void Movement()
+        {
+            switch (currentDirection)
+            {
+                case Direction.Left:
+                    PlayerMovement.X -= speed;
+                    PlayerAngle.Angle = -90;
+                    break;
+                case Direction.Right:
+                    PlayerMovement.X += speed;
+                    PlayerAngle.Angle = 90;
+                    break;
+                case Direction.Up:
+                    PlayerMovement.Y -= speed;
+                    PlayerAngle.Angle = 0;
+                    break;
+                case Direction.Down:
+                    PlayerMovement.Y += speed;
+                    PlayerAngle.Angle = 180;
+                    break;
+                default:
+                    break;
             }
 
 
-            movement();
-
+            Check();
         }
 
-        public  void movement()
+        private void Check()
         {
-            
-               
-                    switch (currentDirection)
-                    {
-                        case Direction.Left:
-                            PlayerMovement.Y += 1;
-                            PlayerAngle.Angle = 90;
-                            break;
-                        case Direction.Right:
-                            PlayerMovement.Y -= 1;
-                            PlayerAngle.Angle = 90;
-                            break;
-                        case Direction.Up:
-                            PlayerMovement.Y -= 1;
-                            PlayerAngle.Angle = -0;
-                            break;
-                        case Direction.Down:
-                            PlayerMovement.Y += 1;
-                            PlayerAngle.Angle = 0;
-                            break;
-                        default: break;
+           
+            Double checkHeight = Wall.ActualHeight / 2;
+            Double checkWidth = Wall.ActualWidth / 2;
 
-                    }
-              
-            if(GameOver)
+            double CurrenXposition = Math.Abs(PlayerMovement.X) + player.Height / 2;
+            double CurrenYposition = Math.Abs(PlayerMovement.Y) + player.Height / 2;
+
+            if (checkHeight < CurrenYposition || checkWidth < CurrenXposition)
             {
-                return;
+                MessageBox.Show("GameOver");
+                ResetGame();
+
+                //ChangeDIR();
             }
-            
-            
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ChangeDIR() 
         {
-            movement();
+            switch (currentDirection)
+            {
+                case Direction.Left:
+                    currentDirection = Direction.Right;
+                    break;
+                case Direction.Right:
+                    currentDirection = Direction.Left;
+                    break;
+                case Direction.Up:
+                    currentDirection = Direction.Down;
+                    break;
+                case Direction.Down:
+                    currentDirection = Direction.Up;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ResetGame()
+        {
+            PlayerMovement.X = 0;
+            PlayerMovement.Y = 0;
+            PlayerAngle.Angle = 0;
+            gameOver = false;
+            currentDirection = Direction.None;
         }
     }
 }
